96a7bd796e9e6adfdc51353fb704756e4954d810
Gilbert Hx <habilbert@gmail.com>
2019-08-06 06:27:06 +0200

    medai and static value

--- pixeleye/settings.py
+++ pixeleye/settings.py
@@ -133,6 +133,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static-root')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static-root')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media-root')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static_in_env")]
