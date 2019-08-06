3d70dd3f213612b5e5649224d063f711e2705fc5
Gilbert Hx <habilbert@gmail.com>
2019-08-06 05:48:09 +0200

    first commit

---
+++ home/views.py
@@ -0,0 +1,12 @@
+from django.views.generic.base import TemplateView
+
+from django.shortcuts import render
+
+# Create your views here.
+class HomePageView(TemplateView):
+
+    template_name = "home.html"
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     return context
