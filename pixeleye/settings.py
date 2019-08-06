1f569f946339590d4cc7cbcbcf079ccd0f265fca
Gilbert Hx <habilbert@gmail.com>
2019-08-06 06:26:01 +0200

    media and statics

--- pixeleye/settings.py
+++ pixeleye/settings.py
@@ -130,10 +130,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static-root/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static-root/')
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static-root')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static-root')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    # '/var/www/static-root/',
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static_in_env")]
